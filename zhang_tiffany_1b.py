03f96c75da71da9e4362bfdb4dbe35f61197c268
tiffany zhang <tiffany@tiffanys-MacBook-Pro-2.local>
2024-03-27 00:52:12 -0400

    My first commit

---
+++ zhang_tiffany_1b.py
@@ -0,0 +1,148 @@
+import turtle
+turtle.setup(700, 700)
+turtle.bgcolor("peach puff")
+turtle.pensize(2)
+turtle.pencolor("white")
+
+turtle.penup()
+turtle.goto(-300, 300)
+turtle.pendown()
+
+#This is The T
+turtle.forward(80)
+turtle.goto(-260, 300)
+turtle.right(90)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(-200, 300)
+turtle.pendown()
+
+#This is the I
+turtle.right(270) 
+turtle.forward(40)
+turtle.goto(-180, 300)
+turtle.right(90)
+turtle.forward(80)
+turtle.penup()
+turtle.goto(-200, 220)
+turtle.pendown()
+turtle.right(270)
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(-130, 300)
+turtle.pendown()
+
+#This is the F
+turtle.right(90)
+turtle.forward(80)
+turtle.penup()
+turtle.goto(-130, 300)
+turtle.pendown()
+turtle.right(270)
+turtle.forward(40)
+turtle.penup()
+turtle.goto(-130, 260)
+turtle.pendown()
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(-60, 300)
+turtle.pendown()
+
+# This is the second F
+turtle.right(90)
+turtle.forward(80)
+turtle.penup()
+turtle.goto(-60, 300)
+turtle.pendown()
+turtle.right(270)
+turtle.forward(40)
+turtle.penup()
+turtle.goto(-60, 260)
+turtle.pendown()
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(20, 300)
+turtle.pendown()
+
+#This is the A
+turtle.forward(40)
+turtle.penup()
+turtle.goto(20,300)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(80)
+turtle.penup()
+turtle.goto(60, 300)
+turtle.pendown()
+turtle.forward(80)
+turtle.penup()
+turtle.goto(20, 260)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(40)
+
+turtle.penup()
+turtle.goto(100, 300)
+turtle.pendown()
+
+#This is the N
+turtle.right(90) 
+turtle.forward(80)
+turtle.penup()
+turtle.goto(100, 300)
+turtle.pendown()
+turtle.left(35)
+turtle.forward(100)
+turtle.penup()
+turtle.goto(156,300)
+turtle.pendown()
+turtle.right(35)
+turtle.forward(82)
+
+turtle.penup()
+turtle.goto(200, 300)
+turtle.pendown()
+
+#This is Y
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.penup()
+turtle.goto(220, 260)
+turtle.pendown()
+turtle.right(180)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(40,-150)
+turtle.pendown()
+turtle.pensize(5)
+turtle.pencolor("mistyrose2")
+
+
+#This is a hexagon
+turtle.fillcolor("lightgrey")
+turtle.begin_fill()
+turtle.right(90)
+turtle.forward(100)
+turtle.right(360/6)
+turtle.forward(100)
+turtle.right(360/6)
+turtle.forward(100)
+turtle.right(360/6)
+turtle.forward(100)
+turtle.right(360/6)
+turtle.forward(100)
+turtle.right(360/6)
+turtle.forward(100)
+turtle.end_fill()
+
+
+
+
